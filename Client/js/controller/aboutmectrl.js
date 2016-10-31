ebf262d46e4b96032d2d3a4693c19982895ca81d
chen xin <chen xin>
2016-10-31 02:31:35 -0400

    add zarfar sign in page

--- Client/js/controller/aboutmectrl.js
+++ Client/js/controller/aboutmectrl.js
@@ -1,8 +1,24 @@
-myApp.controller('AboutMeCtrl', ['$scope', function($scope) {
-	$scope.message = "Hover over to see Info";
+myApp.controller('AboutMeCtrl', ['$scope', '$http', '$state', function($scope, $http, $state) {
+	$scope.message = "Hover over to know about me";
 	$scope.showMsg = function() {
 		console.log("hello");
 	}
+
+	/* static adds right now*/
+	$scope.my_adds = [
+		{
+			"title" : "Testing add 1",
+			"description" : "Testing description 1, this is a static data that just to show how this is will be looked"
+		}
+	]
+	$scope.my_adds.push(
+		{
+			"title" : "Testing add 12",
+			"description" : "Testing description 2, this is a static data that just to show how this is will be looked"
+		}
+	)
+
+
 }]);
 
 myApp.directive('showsMessageWhenHovered', function() {
--- index.php
+++ index.php
@@ -5,21 +5,31 @@
 <!DOCTYPE html>
 <html ng-app='DrakeWeb'>
 <head>
-	<title></title>
-
-	<title>Social App</title>
+	<title>Barking Bazaar</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
     <!-- jquery -->
 	<script src="/UMBC/Client/js/jquery-2.1.4.js"></script>
 
 	<!-- bootstrap & css-->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="/UMBC/Client/css/clear.css">
-	<link rel="stylesheet" type="text/css" href="/UMBC/Client/css/main.css">
+	<!-- main css file -->
+	<link rel="stylesheet" type="text/css" href="/UMBC/Client/css/design.css">
+
+	<!-- google font, zarfar -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Arimo|Merriweather:400,700|Poppins:400,600|Ubuntu:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
 
     <!-- angular -->
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular-resource.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.3.1/angular-ui-router.min.js"></script>
+
 
 
     <!-- self js files, controllers -->
@@ -42,7 +52,7 @@
 					</div>
 
 					<div class='col-sm-2'>
-						<a ui-sref="about"><h2>Service</h2></a>
+						<a ui-sref="service"><h2>Service</h2></a>
 					</div>
 
 					<div class='col-sm-4'>
